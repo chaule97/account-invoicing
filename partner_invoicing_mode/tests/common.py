7c979606c47eee8b45dd6ddd478fcd8a71fac8f0
chaule97 <lebaochau97@gmail.com>
2024-11-08 09:47:54 +0700

    [UPD] partner_invoicing_mode: Update testcase

--- partner_invoicing_mode/tests/common.py
+++ partner_invoicing_mode/tests/common.py
@@ -1,9 +1,10 @@
 # Copyright 2020 Camptocamp SA
 # Copyright 2023 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo.tests.common import TransactionCase
 
 
-class CommonPartnerInvoicingMode:
+class CommonPartnerInvoicingMode(TransactionCase):
     _invoicing_mode = "standard"
 
     @classmethod
